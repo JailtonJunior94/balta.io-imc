b54f7191b36ca8d5b7936f1b22a965c22dc2c065
Jailton Júnior <jailton.junior94@outlook.com>
2020-01-03 21:21:34 -0300

    app sobre imc e bloc

--- lib/ui/android/material.app.dart
+++ lib/ui/android/material.app.dart
@@ -6,7 +6,7 @@ class MyMaterialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'IMC',
+      title: 'App IMC',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.cyan),
       home: HomePage(),
